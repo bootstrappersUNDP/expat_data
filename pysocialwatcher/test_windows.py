ea7f0cae1d8bbb21492e98071e2d24128622d052
bootstrappersUNDP <bootstrappersundp@hotmail.com>
2020-09-24 20:54:41 +0800

    add undp_country script and data

--- pysocialwatcher/test_windows.py
+++ pysocialwatcher/test_windows.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
 	freeze_support()
 	watcher = watcherAPI()
 	watcher.load_credentials_file("pysocialwatcher/credentials.csv")
-	watcher.run_data_collection("pysocialwatcher/input_examples/undp_country.json", "data/")
+	watcher.run_data_collection("pysocialwatcher/input_examples/undp_group.json", "data/")
 	# watcher.print_search_targeting_from_query_dataframe("Lived in Serbia")
 	# watcherAPI.print_geo_locations_given_query_and_location_type(None, ["region"])
